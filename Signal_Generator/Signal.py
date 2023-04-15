49a2b2ed9e637c2754a43ed7b23c03b3dbd605fb
Omar Alkousa <omar.ok1998@gmail.com>
2023-04-15 16:21:45 +0200

    Add Square Signal

--- Signal_Generator/Signal.py
+++ Signal_Generator/Signal.py
@@ -1,13 +1,13 @@
 # Import the required packages
 import numpy as np
 from scipy.signal import chirp
+from scipy.signal import square
 
 
 # Building a class Signal for better use.
 class Signal:
     """
-    Generate sinusoidal signals with specific ampltiudes, frequencies, duration,
-    sampling rate, and phase.
+    Generate sinusoidal signals with specific parameters.
 
     Example:
       signal = Signal(amplitude=10, sampling_rate=2000.0)
@@ -15,16 +15,14 @@ class Signal:
       cosine = signal.cosine()
     """
 
-    def __init__(self, amplitude=1, duration=1, sampling_rate=100.0, phase=0):
+    def __init__(self, amplitude=1, duration=1, sampling_rate=100.0):
         """
         Initialize the Signal class.
 
         Args:
             amplitude (float): The amplitude of the signal
-            frequency (int): The frequency of the signal Hz
             duration (float): The duration of the signal in second
             sampling_rate (float): The sampling per second of the signal
-            phase (float): The phase of the signal in radians
 
         Additional parameters,which are required to generate the signal, are
         calculated and defined to be initialized here too:
@@ -36,37 +34,66 @@ class Signal:
         self.amplitude = amplitude
         self.duration = duration
         self.sampling_rate = sampling_rate
-        self.phase = phase
         self.time_step = 1.0/self.sampling_rate
         self.time_axis = np.arange(0, self.duration, self.time_step)
 
     # Generate sine wave
-    def sine(self, frequency=1.0):
+    def sine(self, frequency=1.0, phase=0):
         """
         Method of Signal
 
+        Args:
+            frequency (float): The frequency of the signal Hz
+            phase (float): The phase of the signal in radians
+
         Returns:
-            np.array of sine wave using the pre-defined variables (amplitude,
-            frequency, time_axis, and phase)
+            np.array of sine wave using the pre-defined variables
         """
-        return self.amplitude*np.sin(2*np.pi*frequency*self.time_axis+self.phase)
+        return self.amplitude*np.sin(2*np.pi*frequency*self.time_axis+phase)
 
     # Generate cosine wave
-    def cosine(self, frequency=1.0):
+    def cosine(self, frequency=1.0, phase=0):
         """
         Method of Signal
 
-        Returns:
-            np.array of cosine wave using the pre-defined variables (amplitude,
-            frequency, time_axis, and phase)
-        """
-        return self.amplitude*np.cos(2*np.pi*frequency*self.time_axis+self.phase)
+        Args:
+            frequency (float): The frequency of the signal Hz
+            phase (float): The phase of the signal in radians
 
-    def chrip_signal(self, frequency_start=1, frequency_end=10, method='linear', vertex=True):
+        Returns:
+            np.array of cosine wave using the pre-defined variables
+        """
+        return self.amplitude*np.cos(2*np.pi*frequency*self.time_axis+phase)
+
+    # Generate chirp signal
+    def chrip_signal(self, frequency_start=1, frequency_end=10, method='linear', vertex=True, phase=0):
+        """
+        Method of Signal
+
+        Args:
+            frequency_start (float): The frequency to start the chirp [Hz]
+            frequency_end (float): The frequency to end the chirp [Hz]
+            method (str): Method to sweep the frequencies ['linear', 'quadratic', 'logarithmic', 'hyperbolic']
+            vertex (bool): Determine whether the vertex of the parabola that is the graph of the frequency is at t=0 or t=duration
+            phase (float): The phase of the signal in radians
+
+        Returns:
+            np.array of chirp wave using the pre-defined variables
+        """
         return self.amplitude*chirp(t=self.time_axis,
                                     f0=frequency_start,
                                     f1=frequency_end,
                                     t1=self.duration,
                                     method=method,
-                                    phi=self.phase*(180/np.pi),
+                                    phi=phase*(180/np.pi),
                                     vertex_zero=vertex)
+
+    # Generate square wave
+    def square_signal(self, frequency=1.0):
+        """
+        Method of Signal
+
+        Returns:
+            np.array of quare wave using the pre-defined variables
+        """
+        return self.amplitude*square(2 * np.pi * frequency * self.time_axis)
