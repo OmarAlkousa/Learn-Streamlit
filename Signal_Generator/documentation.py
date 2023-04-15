1284f7c0069c61a8b50059075754e17acb9166ce
Omar Alkousa <omar.ok1998@gmail.com>
2023-04-15 15:09:28 +0200

    Update Documentation

--- Signal_Generator/documentation.py
+++ Signal_Generator/documentation.py
@@ -8,6 +8,6 @@ def add():
                 unsafe_allow_html=True)
     # Explanation of the web app
     st.markdown('''
-    This web app is to represent how you can use streamlit to interactively generate sinusoidal signals using Numpy package. Also, you can download the
+    This web app is to represent how you can use streamlit to interactively generate sinusoidal signals using Numpy and Scipy packages. Also, you can download the
     generated signal as a csv file.
             ''')
